0ecc7a9f1cf9c004cc2fbc4314a3aeb1c4a36fa7
Obi Somu (Main) <obisomu@gmail.com>
2022-03-20 16:56:24 +0100

    Mutant hog

--- backend/src/config.js
+++ backend/src/config.js
@@ -9,7 +9,7 @@ const network = NETWORK.eth;
 // General metadata for Ethereum
 const namePrefix = "Mutant Hog Coven";
 const description =
-  "This is a collection of 100 unique NFTs residing in the Polygon Chain.";
+  "This is a collection of 100 unique NFTs residing in the Polygon Chain.All members of this coven stands to receive 1000 MHC Token, other bonuses will be revealed by the community soon.";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
@@ -31,7 +31,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://bit.ly/mutanthog", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -42,13 +42,13 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = "rinkeby"; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = "CRYPTOPUNKS";
-const CONTRACT_SYMBOL = "CP";
+const CONTRACT_NAME = "Mutant Hog Coven";
+const CONTRACT_SYMBOL = "MHC";
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = "YOUR WALLET ADDRESS HERE";
-const TREASURY_ADDRESS = "YOUR WALLET ADDRESS HERE";
+const OWNER_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71";
+const TREASURY_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71";
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
@@ -57,10 +57,13 @@ const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required.
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = [
+  0x28e11998a96dcc6aa0a52e4433557547494a0f71,
+  ,
+]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
@@ -68,9 +71,9 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
+const GENERIC_DESCRIPTION = "Which coven card is yours. ?"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE =
-  "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+  "https://ipfs.io/ipfs/bafybeiejpcb5lmq5mqh6ctfdt7zb5sgadwama7ajlnooifthklnszunisy"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -89,10 +92,10 @@ try {
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "https://bit.ly/mutanthog",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71",
       share: 100,
     },
   ],
@@ -119,7 +122,7 @@ const text = {
 };
 
 const pixelFormat = {
-  ratio: 12 / 128,
+  ratio: 21 / 128,
 };
 
 const background = {

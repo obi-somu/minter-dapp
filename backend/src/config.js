f9391814e03bf122f42b9463996d2e752778fbe7
Obi Somu (Main) <obisomu@gmail.com>
2022-03-21 07:32:48 +0100

    test

--- backend/src/config.js
+++ backend/src/config.js
@@ -48,7 +48,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71";
 const TREASURY_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71";
 const MAX_SUPPLY = 1000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.00001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.

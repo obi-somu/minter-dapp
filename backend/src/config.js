a3d52fdcb93112679726dc89d1baaab59a9bb970
Obi Somu (Main) <obisomu@gmail.com>
2022-03-21 06:30:02 +0100

    Fac

--- backend/src/config.js
+++ backend/src/config.js
@@ -56,11 +56,14 @@ const PUBLIC_MINT_START_DATE = "2022-03-30T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_SHARE = 100; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x28e11998A96Dcc6Aa0a52E4433557547494A0F71"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = [
+  "0xfF69c1ee6FeE0F9f0Eedd24eA3fE547F50Cfd10d",
+  ,
+]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
